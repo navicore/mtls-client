f3ffe988f7b24df8bb79a4d14495c15e3d57a55b
Ed Sweeney <ed@onextent.com>
2022-12-22 17:06:40 -0800

    rollback breaking bot changes for 3.xx sb

--- src/test/java/tech/navicore/mtlsclient/MtlsClientApplicationTests.java
+++ src/test/java/tech/navicore/mtlsclient/MtlsClientApplicationTests.java
@@ -3,7 +3,7 @@ package tech.navicore.mtlsclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MtlsClientApplicationTests {
 
 	@Test
